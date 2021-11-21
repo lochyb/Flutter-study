6295793766613c619b4383701c7d3b67f259a2d6
Lochlan <lbernard@nzigen.com>
2021-11-21 18:51:49 +0900

    Revert "Modify Business Card"
    This reverts commit 66793fba853d958b6ac0edff2f3ff81c4185886c.

--- lib/main.dart
+++ lib/main.dart
@@ -11,115 +11,39 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 20.0),
-                decoration: BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                    padding: EdgeInsets.all(25.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundColor: Colors.teal,
-                          radius: 50.0,
-                          backgroundImage:
-                              AssetImage('assets/images/avatar.png'),
-                        ),
-                        SizedBox(height: 10.0),
-                        Text(
-                          'Lochlan Bernard',
-                          style: TextStyle(
-                            fontFamily: 'san-serif bold',
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          'Front End Web Dev',
-                          style: TextStyle(
-                            fontFamily: 'san-serif',
-                            color: Colors.white,
-                          ),
-                        ),
-                        Divider(
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Column(
-                          children: [
-                            Card(
-                              color: Colors.white,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(
-                                      Icons.account_circle,
-                                      color: Colors.teal,
-                                    ),
-                                    SizedBox(
-                                      width: 10.0,
-                                    ),
-                                    Text(
-                                      'www.github.com/lochyb',
-                                      style: TextStyle(
-                                        color: Colors.teal,
-                                        fontFamily: 'san-serif',
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Card(
-                              color: Colors.white,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(
-                                      Icons.location_on,
-                                      color: Colors.teal,
-                                    ),
-                                    SizedBox(
-                                      width: 10.0,
-                                    ),
-                                    Text(
-                                      'Tokyo, Japan',
-                                      style: TextStyle(
-                                        color: Colors.teal,
-                                        fontFamily: 'san-serif',
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )),
-              ),
-            ],
-          ),
-        ),
+            child: Container(
+                child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: 100.0,
+            ),
+            Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.green,
+                    ),
+                  ]),
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+            ),
+          ],
+        ))),
       ),
     );
   }
