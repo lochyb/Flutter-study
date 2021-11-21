993ac5bebf055cca6f76e61c3e934b076f6b5ac3
Lochlan <lbernard@nzigen.com>
2021-11-21 20:16:41 +0900

    Build branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
+  return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
       ),
     ),
   );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
